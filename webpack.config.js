2e0fe99fc636cb41ccb0686c49fbeca29acb456f
ska7 <nikkofann@gmail.com>
2021-07-11 12:26:01 +0300

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./index.html",
+    }),
+  ],
+
+  devServer: {
+    contentBase: path.join(__dirname, "./"),
+    port: 5000,
+  },
+};
